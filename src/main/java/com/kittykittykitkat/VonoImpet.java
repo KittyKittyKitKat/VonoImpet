4488fc21032f21e5ac7e654cb9daff8022bd322a
KittyKittyKitKat <simon.palmer03@gmail.com>
2025-05-20 17:06:09 -0400

    First Commit!

---
+++ src/main/java/com/kittykittykitkat/VonoImpet.java
@@ -0,0 +1,16 @@
+package com.kittykittykitkat;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VonoImpet implements ModInitializer {
+	public static final String MOD_ID = "vono_impet";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
